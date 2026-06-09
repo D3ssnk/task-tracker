a3b6b3a9d7f7b5354286fabc0a0fd0c3bfa69515
D3ssnk <65511755+D3ssnk@users.noreply.github.com>
2026-06-09 14:50:05 +0100

    change variable names to be more concise

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
 import os
+import json
 class TaskManager():
-    def __init__(self,json_path):
-        if not os.path.exists(json_path) or os.path.getsize(json_path) == 0:
-            with open(json_path, "w") as file:
+    def __init__(self,json_file):
+        if not os.path.exists(json_file) or os.path.getsize(json_file) == 0:
+            with open(json_file, "w") as file:
                 file.write('[]')
-        self.json_path = json_path
+        self.json_file = json_file
 
-    def add(self, task):
-        return task
+    def add(self, task_description):
+        with open(self.json_file, "r+") as file:
+            json_data = json.load(file)
+            task_id = len(json_data) + 1
+            task = {"id": task_id, "description": task_description}
+            
+            json_data.append(task) # appends the task to the json list
+            file.seek(0) # sets the cursor to the start so you can rewrite the entire file 
+            json.dump(json_data,file) # writes the data back into the file 
+
 
 def main():
     print("hello world")
--- test_main.py
+++ test_main.py
@@ -5,9 +5,10 @@ import os
 
 @pytest.fixture
 def test_json_file():
-    if not os.path.exists("test_json.json"):
+    if not os.path.exists("test_json.json") or os.path.getsize("test_json.json") != 0:
         with open("test_json.json","w") as file:
             file.write('[]')
+
     yield "test_json.json"
     if os.path.exists("test_json.json"):
         os.remove("test_json.json")
@@ -18,15 +19,19 @@ def test_task_manager(test_json_file):
     return TaskManager(test_json_file)
 
 def test_add(test_task_manager, test_json_file):
-    tmp_tasks = [{'id': 1, 'description': 'Make Coffee'}]
+    test_tasks = [{'id': 1, 'description': 'Make Coffee'}]
     test_task_manager.add("Make Coffee")
 
     with open(test_json_file, "r") as file:
         file_data = json.load(file)
 
-    assert file_data == tmp_tasks, "Correctly adds task with ID as one"
+    assert file_data == test_tasks, "Correctly adds task with ID as one"
 
+        
     test_task_manager.add("Make Tea")
-    tmp_tasks.append({'id': 2, 'description': 'Make Tea'})
+    test_tasks.append({'id': 2, 'description': 'Make Tea'})
 
-    assert file_data == tmp_tasks, "Correctly adds task with incremented ID"
+    with open(test_json_file, "r") as file:
+        file_data = json.load(file)
+
+    assert file_data == test_tasks, "Correctly adds task with incremented ID"
